d34dcd2c36c49b4f7d75f9e04a92066b5f451884
David Alencar <davidalencarignacio@gmail.com>
2022-02-05 16:42:15 -0300

    reciving msg from client and broadcast

--- lib/rumbl_web/channels/videos_channel.ex
+++ lib/rumbl_web/channels/videos_channel.ex
@@ -6,11 +6,22 @@ defmodule RumblWeb.VideosChannel do
       {:ok, assign(socket, :video_id, String.to_integer(video_id))}
   end
 
+  def handle_info(:ping, socket) do
+    count = socket.assigns[:count] || 1
+    push(socket, "ping", %{count: count})
+    {:noreply, assign(socket, :count, count + 1)}
+  end
+
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("new_annotation", payload, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: payload["body"],
+      at: payload["at"]
+    })
+    {:reply, :ok, socket}
   end
 
   # It is also common to receive messages from the client and
